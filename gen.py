d44e856305e98206bfceca648e39ff11c8f0fb04
YazdonovJ <jamo1iddingrozniy@gmail.com>
2025-08-23 02:33:29 +0500

    Update gen.py

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ import base64
 import uuid
 from tokens import GEMINI_API
 
-client = genai.Client(api_key=GEMINI_API)
+client = genai.Client(AIzaSyBM5ERNIug2sLF38gAHhv8NBAhQCo0lvLM)
 
 def generate_image(text):
     try:
